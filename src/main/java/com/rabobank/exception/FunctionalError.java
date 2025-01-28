d82955c161cf1807bae4aefeb9ef777a0068ca57
SakshiMahajan899 <mahajansakshi899@gmail.com>
2025-01-28 09:40:17 +0100

    Update FunctionalError.java

--- src/main/java/com/rabobank/exception/FunctionalError.java
+++ src/main/java/com/rabobank/exception/FunctionalError.java
@@ -10,10 +10,13 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum FunctionalError {
 
-	INVALID_CARD_TYPE("FUN_ERR_001", "Invalid card type"), INVALID_AMOUNT("ERR_002", "Invalid amount"),
-	INVALID_ACCOUNT_NUMBER("FUN_ERR_003", "Invalid account number"), INVALID_CUSTOMER_ID("ERR_004", "Invalid customer ID"),
+	INVALID_CARD_TYPE("FUN_ERR_001", "Invalid card type"), 
+	INVALID_AMOUNT("FUN_ERR_002", "Invalid amount"),
+	INVALID_ACCOUNT_NUMBER("FUN_ERR_003", "Invalid account number"), 
+	INVALID_CUSTOMER_ID("FUN_ERR_004", "Invalid customer ID"),
 	INSUFFICIENT_BALANCE("FUN_ERR_005", "Insufficient funds in the account"),
-	ACCOUNT_NOT_FOUND("FUN_ERR_006", "No Account Present in the database"), ILLEGAL_ARG("ERR_007", "Ilegal Argument ");
+	ACCOUNT_NOT_FOUND("FUN_ERR_006", "No Account Present in the database"),
+	ILLEGAL_ARG("FUN_ERR_007", "Ilegal Argument ");
 
 	private final String code;
 	private final String errorMessage;
